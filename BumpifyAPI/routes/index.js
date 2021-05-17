65f7c55d3318b9aa1472764482c65af9cb2faeb2
Mario Lopez <00046317@uca.edu.sv>
2021-05-16 18:31:46 -0600

    Added .env, nodemon and connection to DB.

--- BumpifyAPI/routes/index.js
+++ BumpifyAPI/routes/index.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var dbClient = require('../utils/conn');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
+
+  dbClient.connect(error => {
+    const collection = dbClient.db(process.env.DATABASE_NAME).collection("test");
+
+    collection.insertOne({ test: "Yes" })
+      .then(res => {
+        console.log("Wena bro.")
+        dbClient.close();
+      })
+      .catch(err => {
+        console.log(err)
+      });
+  });
+
   res.render('index', { title: 'Express' });
 });
 
