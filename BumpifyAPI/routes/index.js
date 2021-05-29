6a4fdc9b0eec61b6345a5467ae171e7146b2a3d9
Mario Lopez <00046317@uca.edu.sv>
2021-05-29 17:04:03 -0600

    API GET Request base

--- BumpifyAPI/routes/index.js
+++ BumpifyAPI/routes/index.js
@@ -1,11 +1,37 @@
 var express = require('express');
 var router = express.Router();
-var dbClient = require('../utils/conn');
+var db = require('../utils/conn');
 
-/* GET home page. */
+router.get('/test', function (req, res, next) {
+  let client = db.client();
+
+  client.connect(err => {
+    if (err) res.json(err).status(500);
+
+    const collection = client.db(process.env.DATABASE_NAME_T).collection("shipwrecks");
+
+    collection.find().limit(10)
+      .toArray((err, result) => {
+
+        if (err) res.json(err).status(500);
+
+        res.json(result).status(200);
+        client.close();
+
+      });
+  });
+
+});
+
 router.get('/', function (req, res, next) {
+  
+  res.render('index', { title: 'Bumpify'});
 
-  dbClient.connect(error => {
+});
+
+module.exports = router;
+
+/*dbClient.connect(error => {
     const collection = dbClient.db(process.env.DATABASE_NAME).collection("test");
 
     collection.insertOne({ test: "Yes" })
@@ -16,9 +42,4 @@ router.get('/', function (req, res, next) {
       .catch(err => {
         console.log(err)
       });
-  });
-
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+  });*/
--- BumpifyAPI/utils/conn.js
+++ BumpifyAPI/utils/conn.js
@@ -1,5 +1,8 @@
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://" + process.env.DATABASE_USER_NAME + ":" + process.env.DATABASE_PASSWORD + "@bumpifydb.tslk6.mongodb.net/" + process.env.DATABASE_NAME + "?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-module.exports = client
+function client(){
+    return new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+}
+
+module.exports = {client}
