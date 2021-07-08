537ed6e8f4a70de9633482f0598b9496ef87c6c5
Victor <00046517@uca.edu.sv>
2021-07-07 18:25:01 -0600

    More documentation

--- Bumpify/app/src/main/java/com/example/bumpify/MainViewModel.kt
+++ Bumpify/app/src/main/java/com/example/bumpify/MainViewModel.kt
@@ -1,6 +1,23 @@
+/*
+  Licensed to the Apache Software Foundation (ASF) under one
+  or more contributor license agreements.  See the NOTICE file
+  distributed with this work for additional information
+  regarding copyright ownership.  The ASF licenses this file
+  to you under the Apache License, Version 2.0 (the
+  "License"); you may not use this file except in compliance
+  with the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing,
+  software distributed under the License is distributed on an
+  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+  KIND, either express or implied.  See the License for the
+  specific language governing permissions and limitations
+  under the License.
+ */
 package com.example.bumpify
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -8,7 +25,6 @@ import com.example.bumpify.model.*
 import com.example.bumpify.repository.Repository
 import kotlinx.coroutines.launch
 import retrofit2.Response
-import java.util.*
 
 /**
  * Código Boilerplate necesario para el uso de la libreria Retrofit
